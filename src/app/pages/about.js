9a4b41cfdd95e2bf35d8167d2751ed163d475a50
Demetrio <dmitr77@gmail.com>
2019-03-18 00:24:50 -0500

    on my way

---
+++ src/app/pages/about.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+
+export default () => (
+  <div>
+    <Layout>
+      <p>Hello from about</p>
+    </Layout>
+  </div>
+)
--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -12,15 +12,12 @@
 // serviceWorker.unregister();
 
 // This is the Link API
-import Link from 'next/link'
+import Layout from '../components/Layout'
 
-const Index = () => (
+export default () => (
   <div>
-    <Link href="/about">
-      <button>Go to About Page</button>
-    </Link>
-    <p>Hello Next.js</p>
+    <Layout >
+      <p>Hello Next.js</p>
+    </Layout>
   </div>
 )
-
-export default Index
