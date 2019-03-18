62d8d50ce4719c1e62da909f94c5cf5f5ae8b692
dxu8awy <demetrio_urquidi@homedepot.com>
2019-03-18 13:13:25 -0500

    added a server using express

--- src/app/pages/about.js
+++ src/app/pages/about.js
@@ -1,9 +1,14 @@
 import Layout from '../components/Layout'
 
+const sayHi = () => {
+  console.log('hi')
+}
+
 export default () => (
   <div>
     <Layout>
       <p>Hello from about</p>
+      <button onClick={sayHi} />
     </Layout>
   </div>
 )
--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -12,12 +12,45 @@
 // serviceWorker.unregister();
 
 // This is the Link API
-import Layout from '../components/Layout'
+import React, { Component } from 'react';
+import Layout from '../components/Layout.js'
+import Link from 'next/link'
 
-export default () => (
-  <div>
-    <Layout >
-      <p>Hello Next.js</p>
-    </Layout>
-  </div>
-)
+class Index extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      users: []
+    };
+  }
+
+  componentWillMount() {
+    this.setState({
+      users: ["guy1", "guy2"]
+    });
+  }
+
+  render() {
+    const PostLink = props => (
+      <li>
+        <Link as={`/p/${props.id}`} href={`/post?title=${props.id}`}>
+            <a>{props.title}</a>
+        </Link>
+      </li>
+    );
+
+    return (
+      <Layout>
+        <h1>My Blog</h1>
+        <ul>
+          <PostLink id="hello-nextjs" title="Hello Next.js" />
+          <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
+          <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+        </ul>
+      </Layout>
+    );
+  }
+}
+
+export default Index;
