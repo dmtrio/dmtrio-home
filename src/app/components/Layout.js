f7ed58123b2ddf1827b55914787ab82ff7dc353b
dxu8awy <demetrio_urquidi@homedepot.com>
2019-03-26 15:55:08 -0500

    addoing scss

--- src/app/components/Layout.js
+++ src/app/components/Layout.js
@@ -1,17 +1,14 @@
 import ExtendHead from './ExtendHead';
 import Header from './Header';
 
-const layoutStyle = {
-  margin: 20,
-  padding: 20,
-  border: '1px solid #DDD'
-}
 
 const Layout = props => (
   <div>
     <ExtendHead css={props.css} />
-    <h2>This is a Serveless Server-Side Rendered app</h2>
-    <div style={layoutStyle}>
+    <div className="banner">
+      <h2>This is a Serveless Server-Side Rendered app</h2>
+    </div>
+    <div className="page-layout">
       <Header />
       {props.children}
     </div>
